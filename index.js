b70c16a9755e453effa70af92e7ea48468ae2cc7
Aleksandr <viking26@mail.ru>
2022-06-20 15:27:12 +0300

    test

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('/*', (req, res) => {
     req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
-}
 // const indexRouter = require('./routes/indexRouter');
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
